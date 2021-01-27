86d6a96f5aae2a09626b64efc027c9433a34784f
Liam Erickson <71674831+ericksonl@users.noreply.github.com>
2021-01-26 20:04:57 -0800

    Add files via upload

---
+++ Queue.java
@@ -0,0 +1,63 @@
+import Stack.Node;
+
+/* Author: Liam Erickson
+ * File: Queue.java
+ * Date: May 28, 2020
+ * Class: CS258 - Lab 6 Part 2
+ */
+
+public class Queue<T> {
+
+	private Node first = null; // top of stack
+	private Node last = null;
+	private int n = 0;
+
+	class Node {
+		T value;
+		Node next;
+	}
+
+	public void enqueue(T value) {
+		Node temp = new Node();
+		temp.value = value;
+		temp.next = null;
+		if (last == null) {
+			last = temp;
+			first = temp;
+		} else {
+			last.next = temp;
+			last = temp;
+		}
+		n++;
+	}
+
+	public T dequeue() {
+		Node temp = first;
+		first = temp.next;
+		if (first == null)
+			last = null;
+		n--;
+		return temp.value;
+	}
+
+	public T first() {
+		return first.value;
+	}
+
+	public int count() {
+		return n;
+	}
+
+	public boolean isEmpty() {
+		return (first == null);
+	}
+	
+	public String toString() {
+		String s = "" + top.value;
+		Node current = top.next;
+		while (current != null) {
+			s += ", " + current.value;
+			current = current.next;
+		}
+		return s;
+}
